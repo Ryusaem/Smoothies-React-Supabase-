18d6646bde93cd8232a3d3c90a8a26f7700a2270
Ryusaem <53605044+Ryusaem@users.noreply.github.com>
2023-06-22 20:42:27 +0200

    First commit: adding the project to github

---
+++ src/pages/Home.js
@@ -0,0 +1,54 @@
+import supabase from '../config/supabaseClient'
+import { useEffect, useState } from 'react'
+
+// components
+import SmoothieCard from '../components/SmoothieCard'
+
+const Home = () => {
+  const [fetchError, setFetchError] = useState(null)
+  const [smoothies, setSmoothies] = useState(null)
+
+  const handleDelete = (id) => {
+    setSmoothies(prevSmoothies => {
+      return prevSmoothies.filter(prevSmoothie => prevSmoothie.id !== id)
+    })
+  }
+
+  useEffect(() => {
+    const fetchSmoothies = async () => {
+      const { data, error } = await supabase
+        .from('smoothies')
+        .select()
+      
+      if (error) {
+        setFetchError('Could not fetch the smoothies')
+        setSmoothies(null)
+      }
+      if (data) {
+        setSmoothies(data)
+        setFetchError(null)
+      }
+    }
+
+    fetchSmoothies()
+
+  }, [])
+
+  return (
+    <div className="page home">
+      {fetchError && (<p>{fetchError}</p>)}
+      {smoothies && (
+        <div className="smoothies">
+          {/* order-by buttons */}
+          <div className="smoothie-grid">
+            {smoothies.map(smoothie => (
+              <SmoothieCard key={smoothie.id} smoothie={smoothie} onDelete={handleDelete} />
+            ))}
+          </div>
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default Home
